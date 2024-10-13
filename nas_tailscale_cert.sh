c26f6e11ef48971f6af401a8ad751fa0e5da8910
James Goodhouse <4684194+jamesgoodhouse@users.noreply.github.com>
2024-10-13 11:00:09 -0700

    Update nas_tailscale_cert.sh (#1)

--- nas_tailscale_cert.sh
+++ nas_tailscale_cert.sh
@@ -20,6 +20,13 @@ copy_if_different() {
   fi
 }
 
+restart_services() {
+  echo "Restarting services"
+  /bin/systemctl restart nginx
+  /bin/systemctl restart pkgctl-WebDAVServer
+  echo "Services restarted"
+}
+
 if [ "$EUID" -ne 0 ]; then
   echo "This script must be run as root"
   exit 1
@@ -29,10 +36,22 @@ USER_HOME=$(eval echo ~$SUDO_USER)
 TEMPDIR="$USER_HOME/.tailscale_certs"
 TS_DNS=$(/usr/local/bin/tailscale status --json | jq -r '.Self.DNSName | .[:-1]')
 CERT_DIR="/usr/syno/etc/certificate/_archive"
-DEFAULT_CERT_ID=$(cat $CERT_DIR/DEFAULT)
+DEFAULT_FILE_PATH="$CERT_DIR/DEFAULT"
+DEFAULT_CERT_ID=$(cat "$DEFAULT_FILE_PATH")
+INFO_FILE_PATH="$CERT_DIR/INFO"
+SERVICES_FILE_PATH="$CERT_DIR/SERVICES"
 TAILSCALE_CERT_ID=tailscale
 TAILSCALE_CERT_DIR="$CERT_DIR/$TAILSCALE_CERT_ID"
+
 CERTIFICATES_UPDATED=0
+
+if ! jq -e ". | has(\"$TAILSCALE_CERT_ID\")" "$INFO_FILE_PATH" > /dev/null; then
+  echo "Adding Tailscale to $INFO_FILE_PATH"
+  cat "$INFO_FILE_PATH" | jq --arg key "$TAILSCALE_CERT_ID" --argjson services "$(cat $SERVICES_FILE_PATH)" \
+               '. + {$key: {"desc": "Tailscale", "services": $services}}' > "$INFO_FILE_PATH"
+else
+  echo "Tailscale already added to $INFO_FILE_PATH"
+fi
 
 mkdir -p "$TEMPDIR"
 trap "rm -rf $TEMPDIR" EXIT
@@ -43,19 +62,18 @@ echo "Fetching certificates"
 echo "Converting to PKCS #8 format"
 /bin/openssl pkcs8 -topk8 -nocrypt -in "$TEMPDIR/$TS_DNS.key" -out "$TEMPDIR/p8file.pem"
 
+mkdir -p "$TAILSCALE_CERT_DIR"
 copy_if_different "$TEMPDIR/$TS_DNS.crt" "$TAILSCALE_CERT_DIR/cert.pem" && CERTIFICATES_UPDATED=1
 copy_if_different "$TEMPDIR/$TS_DNS.crt" "$TAILSCALE_CERT_DIR/fullchain.pem" && CERTIFICATES_UPDATED=1
 copy_if_different "$TEMPDIR/p8file.pem" "$TAILSCALE_CERT_DIR/privkey.pem" && CERTIFICATES_UPDATED=1
 
+if [ "$DEFAULT_CERT_ID" != "$TAILSCALE_CERT_ID" ]; then
+  echo "$TAILSCALE_CERT_ID" > "$DEFAULT_FILE_PATH"
+fi
+
 if [ "$CERTIFICATES_UPDATED" -eq 1 ]; then
   echo "Certificates updated"
-
-  if [ "$DEFAULT_CERT_ID" = "$TAILSCALE_CERT_ID" ]; then
-    echo "Restarting services"
-    /bin/systemctl restart nginx
-    /bin/systemctl restart pkgctl-WebDAVServer
-    echo "Services restarted"
-  fi
+  restart_services
 else
   echo "Certificates up-to-date"
 fi
